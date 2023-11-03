5859111a3cdd4c1a93721a9f12e41065551ac7da
Nico Jensch <root@dr460nf1r3.org>
2023-11-03 20:40:16 +0100

    chore: update PKGBUILDs to latest versions

--- garuda-fish-config/config.fish
+++ garuda-fish-config/config.fish
@@ -88,7 +88,7 @@ end
 function copy
     set count (count $argv | tr -d \n)
     if test "$count" = 2; and test -d "$argv[1]"
-	set from (echo $argv[1] | trim-right /)
+	set from (echo $argv[1] | string trim --right --chars=/)
 	set to (echo $argv[2])
         command cp -r $from $to
     else
