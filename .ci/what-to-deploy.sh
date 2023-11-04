a296162968b66d4077d56da78e4320498c47d33c
Nico Jensch <root@dr460nf1r3.org>
2023-11-04 20:01:06 +0100

    ci: use another regex [deploy garuda-bash-config]

--- .ci/what-to-deploy.sh
+++ .ci/what-to-deploy.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
-mapfile -t _PACKAGES < <(find . -type d | sed 's|^./||')
+mapfile -t _PACKAGES < <(find . -mindepth 1 -type d -prune | sed 's|^./||')
 
-[[ "$CI_COMMIT_MESSAGE" =~ "deploy all" ]] && TO_DEPLOY="routine" && exit 0
+[[ "$CI_COMMIT_MESSAGE" == *"[deploy all]" ]] &&
+    echo "routine" >> /tmp/TO_DEPLOY && 
+    echo "Requested a full routine run." &&
+    exit 0
 
 for i in "${_PACKAGES[@]}"; do
-    [[ "$CI_COMMIT_MESSAGE" =~ "deploy $i" ]] && TO_DEPLOY="$i" && exit 0
+    # shellcheck disable=SC2076
+    [[ "$CI_COMMIT_MESSAGE" == *"[deploy $i]"* ]] &&
+        echo "$i" >> /tmp/TO_DEPLOY && 
+        echo "Requested package build for $TO_DEPLOY." &&
+        exit 0
 done
 
 echo "No package to deploy found in commit message, aborting." && exit 1
