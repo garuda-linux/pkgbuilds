cd2c8d0ee624c159a51d76b2e6409503f2431bbe
Nico Jensch <root@dr460nf1r3.org>
2024-01-13 18:19:01 +0100

    ci: import chaotic-poc and drop redundant files
    Additionally we have gitlab as CI_PKGBUILD_SOURCE, replacing the SOURCES file.

--- garuda-browser-settings/librewolf/all-companyname.js
+++
@@ -1,3 +0,0 @@
-// Home page :
-// https://developer.mozilla.org/en-US/docs/Mozilla/Preferences/A_brief_guide_to_Mozilla_preferences#Modifying_preferences
-pref("browser.startup.homepage", "https://start.garudalinux.org/");
