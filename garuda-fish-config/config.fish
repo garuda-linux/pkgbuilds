59a0eca919a33c6f3277e7aef0e302fae3c10bbb
BluishHumility <bluishhumility@duck.com>
2024-02-03 22:34:53 -0500

    fix(garuda-fish-config): fix man page formatting issue
    set MANROFFOPT="-c" to fix the man page formating isue with bat.
    Reference: https://github.com/sharkdp/bat?tab=readme-ov-file#man

--- garuda-fish-config/config.fish
+++ garuda-fish-config/config.fish
@@ -2,7 +2,8 @@
 # Hide welcome message & ensure we are reporting fish as shell
 set fish_greeting
 set VIRTUAL_ENV_DISABLE_PROMPT "1"
-set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+set -xU MANPAGER "sh -c 'col -bx | bat -l man -p'"
+set -xU MANROFFOPT "-c"
 set -x SHELL /usr/bin/fish
 
 ## Export variable need for qt-theme
