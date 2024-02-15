477b10967929373f9141eb30637806ddb79440a0
GitLab CI <ci@garudalinux.org>
2024-02-15 07:03:52 +0000

    chore(ci): update CI from template

---
+++ .ci/manual-schedule.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+set -x
+
+source .ci/util.shlib
+
+# This script is used to manually trigger a build via pipeline UI
+# PACKAGES is a ":" separated list of package names and needs
+# to be supplied via pipeline variables
+
+# Read config file into global variables
+UTIL_READ_CONFIG_FILE
+
+if [ "$PACKAGES" == "all" ]; then
+    .ci/schedule-packages.sh schedule all
+    exit 0
+fi
+
+declare -a FINAL=()
+IFS=':' read -r -a FINAL <<<"$PACKAGES"
+.ci/schedule-packages.sh schedule "${FINAL[@]}"
