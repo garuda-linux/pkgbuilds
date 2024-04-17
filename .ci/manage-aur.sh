67c014f8d77b739f8f0abff93d24bf3674a174a5
GitLab CI <ci@garudalinux.org>
2024-04-17 06:55:41 +0000

    chore(ci): update CI from template

--- .ci/manage-aur.sh
+++ .ci/manage-aur.sh
@@ -2,8 +2,12 @@
 # shellcheck disable=2153
 set -euo pipefail
 
+# Set up required environment
 [[ -v "TMPDIR" ]] || TMPDIR="/tmp"
 mkdir -p "$TMPDIR/aur-push"
+
+git config --global user.name "$GIT_AUTHOR_NAME"
+git config --global user.email "$GIT_AUTHOR_EMAIL"
 
 # This script is used to manage a packages corresponding AUR repo
 # No point in running this script if we don't have keys available
@@ -67,9 +71,12 @@ for package in "${PACKAGES[@]}"; do
         fi
 
         # We always run shfmt on the PKGBUILD. Two runs of shfmt on the same file should not change anything
+        if [ -f "$TMPDIR/aur-push/$package/.editorconfig" ]; then
+            cp "./.editorconfig" "$TMPDIR/aur-push/$package/.editorconfig"
+        fi
         shfmt -w "$TMPDIR/aur-push/$package/PKGBUILD"
 
-        # Rsync: delete files in the destination that are not in the source. Exclude copying .CI and .git            
+        # Rsync: delete files in the destination that are not in the source. Exclude copying .CI and .git
         # shellcheck disable=SC2046
         rsync -av --delete $(UTIL_GET_EXCLUDE_LIST "--exclude") "$package/" "$TMPDIR/aur-push/$package/"
 
