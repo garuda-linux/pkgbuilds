26d7f8496c242265b6902ee9188f134194cd0c0b
TNE <tne@garudalinux.org>
2024-04-28 02:06:40 +0200

    feat(garuda-dracut-support): Support linux 6.8
    Explicitly remount the root btrfs volume as rw, so subvolumes can be rw if they desire to be.
    https://lore.kernel.org/linux-btrfs/0a73915edbb8d05e30d167351ea8c709a9bfe447.1699308010.git.josef@toxicpanda.com/

--- garuda-dracut-support/snapshot-overlay.sh
+++ garuda-dracut-support/snapshot-overlay.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 function mount_snapshot_overlay() {
 	local root_mnt="$NEWROOT"
-	if [[ "$(findmnt --mountpoint "$root_mnt" -o FSTYPE -n)" = "btrfs" ]] && [[ "$(btrfs property get "${root_mnt}" ro)" != "ro=false" ]]; then
+	local UUID FSTYPE
+	IFS=" " read -r UUID FSTYPE < <(findmnt --mountpoint "$root_mnt" -o UUID,FSTYPE -n)
+	if [[ "$FSTYPE" = "btrfs" ]] && [[ "$(btrfs property get "${root_mnt}" ro)" == "ro=true" ]]; then
 		local ram_dir
 		ram_dir=$(mktemp -d -p /)
+
+		# Because of https://lore.kernel.org/linux-btrfs/0a73915edbb8d05e30d167351ea8c709a9bfe447.1699308010.git.josef@toxicpanda.com/ with more info at https://gitlab.com/garuda-linux/pkgbuilds/-/issues/10
+		# we are required to mount the top btrfs volume and then remount it as read-write explicitly to avoid all submodules being RO
+		mount -t btrfs "UUID=${UUID}" "${ram_dir}"
+		mount -o remount,rw "${ram_dir}"
+		umount "${ram_dir}"
+
 		mount -t tmpfs cowspace "${ram_dir}"
 		mkdir -p "${ram_dir}"/{upper,work}
 		mount -t overlay -o lowerdir="${root_mnt}",upperdir="${ram_dir}"/upper,workdir="${ram_dir}"/work rootfs "${root_mnt}"
