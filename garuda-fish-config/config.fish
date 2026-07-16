6063c34ce7d35045c8e3a6481f743a9f142c61e6
TNE <tne@garudalinux.org>
2026-07-16 02:46:58 +0200

    fix(shells): Update eza alias for updated flag syntax

--- garuda-fish-config/config.fish
+++ garuda-fish-config/config.fish
@@ -102,12 +102,12 @@ end
 
 ## Useful aliases
 # Replace ls with eza
-alias ls 'eza -al --color=always --group-directories-first --icons' # preferred listing
-alias lsz 'eza -al --color=always --total-size --group-directories-first --icons' # include file size
-alias la 'eza -a --color=always --group-directories-first --icons'  # all files and dirs
-alias ll 'eza -l --color=always --group-directories-first --icons'  # long format
-alias lt 'eza -aT --color=always --group-directories-first --icons' # tree listing
-alias l. 'eza -ald --color=always --group-directories-first --icons .*' # show only dotfiles
+alias ls 'eza -al --group-directories-first' # preferred listing
+alias lsz 'eza -al --total-size --group-directories-first' # include file size
+alias la 'eza -a --group-directories-first'  # all files and dirs
+alias ll 'eza -l --group-directories-first'  # long format
+alias lt 'eza -aT --group-directories-first' # tree listing
+alias l. 'eza -ald --group-directories-first .*' # show only dotfiles
 
 # Replace some more things with better alternatives
 abbr cat 'bat --style header,snip,changes'
